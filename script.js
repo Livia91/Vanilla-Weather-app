d31e213e42a5acb11ee5662c8606144602d86214
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-08-03 12:37:48 +0100

    current data
    I have added the current temperature

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ function displayTemperature (response) {
    let temperatureElement = document.querySelector("#temperature");
    let cityElement = document.querySelector("#city");
    let descriptionElement = document.querySelector("#description");
+   let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
  temperatureElement.innerHTML = Math.round (response.data.main.temp);
  cityElement.innerHTML = response.data.name;
  descriptionElement.innerHTML = response.data.weather[0].description;
+ humidityElement.innerHTML = response.data.main.humitidy;
+ windElement.innerHTML = Math.round (response.data.wind.speed);
 }
 
 
