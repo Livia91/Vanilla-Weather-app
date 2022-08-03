e469cc63321e44e5301234903ec4d8b403b3ac14
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-08-03 13:55:51 +0100

    update the time

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function formateDate(timestamp) {
+ let date = new Date(timestamp);
+ let hours = date.getHours();
+ if (hours < 10) {
+  hours = `0${hours}`;
+}
+ let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+ let days = [
+  "Sunday", 
+  "Tuesday", 
+  "Wednesday", 
+  "Thursday", 
+  "Friday",
+  "Saturday"];
+
+ let day = date.getDay();
+ return `${day} ${hours}:${minutes}`;
+}
+
+
+
+
+
 function displayTemperature (response) { 
     console.log(response.data);
    let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +31,13 @@ function displayTemperature (response) {
    let descriptionElement = document.querySelector("#description");
    let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
  temperatureElement.innerHTML = Math.round (response.data.main.temp);
  cityElement.innerHTML = response.data.name;
  descriptionElement.innerHTML = response.data.weather[0].description;
  humidityElement.innerHTML = response.data.main.humitidy;
  windElement.innerHTML = Math.round (response.data.wind.speed);
+ dateElement.innerHTML = formateDate(response.data.dt);
 }
 
 
