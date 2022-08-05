122124be2f5ea1e953040beca60ec2d225bd23d1
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-08-05 20:01:26 +0100

    added conversion from ℃ to ℉

--- src/script.js
+++ src/script.js
@@ -67,12 +67,16 @@ function handleSubmit(event) {
 function displayFahrenheitTemperature(event) {
  event.preventDefault(); 
  let temperatureElement = document.querySelector("#temperature");
+ celsiusLink.classList.remove("active");
+ fahrenheitLink.classList.add("active");
  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
  
 function displayCelsiusTemperature(event) {
 event.preventDefault(); 
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
 let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
