70fc5a2b94fc8582947a2d53ee0f274480303684
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-08-05 17:52:35 +0100

    update

--- src/script.js
+++ src/script.js
@@ -70,6 +70,12 @@ function displayFahrenheitTemperature(event) {
  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
+ 
+function displayCelsiusTemperature(event) {
+event.preventDefault(); 
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
 let celsiusTemperature = null;
 
@@ -78,5 +84,8 @@ form.addEventListener("submit", handleSubmit);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Harrow");
