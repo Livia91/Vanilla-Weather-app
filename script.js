5334ed3fefb682e137401bfc06ee7b8bc51298eb
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-08-13 16:09:29 +0100

    API Integration
    I have finalized the API integration with the current weather and display the next 6 days of the weather.

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ if (minutes < 10) {
  return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+
+return days[day];
+}
+
 
 function dispalyForecast(response) {
   let forecast = response.data.daily;
@@ -29,19 +37,22 @@ function dispalyForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
  let forecastHTML = `<div class="row">`;
- forecast.forEach(function(forecastDay) {
+ forecast.forEach(function(forecastDay, index) {
+  if (index < 6) {
  forecastHTML = forecastHTML +  `
       <div class="col-2">
-        <div class="weather-forecast-date">${forecastDay.dt}</div>
+        <div class="weather-forecast-date">${formatDay (forecastDay.dt)}</div>
         <img
-          src="https://openweathermap.org/img/wn/${forecastDay.weather[o].icon}@2x.png"
+          src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="36" />
           <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+
+          <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>`;
+  }
  });
   forecastHTML = forecastHTML + `</div>`;
 
