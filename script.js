bc3c788231330b2d03d91e59dca26990dde451e5
Livia91 <109625994+Livia91@users.noreply.github.com>
2022-08-13 16:30:13 +0100

    update weather description
    is added the humidity

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ function displayTemperature (response) {
    let temperatureElement = document.querySelector("#temperature");
    let cityElement = document.querySelector("#city");
    let descriptionElement = document.querySelector("#description");
+   let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
@@ -82,6 +83,7 @@ celsiusTemperature = response.data.main.temp;
  temperatureElement.innerHTML = Math.round (response.data.main.temp);
  cityElement.innerHTML = response.data.name;
  descriptionElement.innerHTML = response.data.weather[0].description;
+ humidityElement.innerHTML = response.data.main.humidity;
  windElement.innerHTML = Math.round (response.data.wind.speed);
  dateElement.innerHTML = formateDate(response.data.dt);
  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
